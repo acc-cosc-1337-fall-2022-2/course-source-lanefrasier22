91449c59ef10e0546f56c5bd7538aa91d24d75cf
lanefrasier22 <lane.frasier@g.austincc.edu>
2022-09-16 18:19:38 +0000

    hw2 with test cases

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -1,8 +1,12 @@
 ﻿//example
 int add_numbers(int num1, int num2);
 
-//write function prototype here
 
+
+//write function prototype here
+const double tax_rate = 0.0675;
+double get_sales_tax_amount(double meal_amount);
+double get_tip_amount(double meal_amount, double tip_rate);
 
 
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,15 +1,36 @@
 ﻿//write include statements
-
+#include "hwexpressions.h"
+#include <iostream>
 
 //write namespace using statement for cout
-
+using namespace std;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	double meal_amount, tip_rate, tip_amount, tax_amount, total;
+
+	cout<<"Please enter your meal amount: ";
+	cin>>meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+
+	cout<<"Please enter the percentage of tip you'd like to provide (in decimal"
+	" form): \n";
+	cin>>tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+
+	total = tip_amount + tax_amount + meal_amount;
+
+	cout<<"Here is your receipt: \n"
+	"Meal amount is $"<<meal_amount<<
+	"\nSales tax is "<<tax_rate * 100<<"%"
+	"\nTip amount is $"<<tip_amount<<
+	"\nTotal is $"<<total<<"\n";
+
 
 	return 0;
 }
