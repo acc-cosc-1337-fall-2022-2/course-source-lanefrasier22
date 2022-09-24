7c3b691f96314956cb6340724f300cf63ff276d2
lanefrasier22 <lane.frasier@g.austincc.edu>
2022-09-24 00:38:24 +0000

    hw 3 with test cases

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,40 @@
 //write include statement for decisions header
-
+#include "decisions.h"
+using namespace std;
+#include <string>
 
 //Write code for function(s) code here
+string get_letter_grade_using_if(int num)
+{
+    if (num >= 90 && num <=100){
+        return "A";
+    }else if ( num >=80 && num < 90){
+        return "B";
+    }else if (num >= 70 && num < 80){
+        return "C";
+    }else if (num >= 60 && num < 70){
+        return "D";
+    }else return "F";
+}
+
+string get_letter_grade_using_switch(int num)
+{
+    switch(num/10){
+        case 10:
+            return "A";
+            break;
+        case 9:
+            return "A";
+            break;
+        case 8:
+            return "B";
+            break;
+        case 7:
+            return "C";
+            break;
+        case 6:
+            return "D";
+        default:
+            return "F";
+    }
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,21 @@
 //write include statements
+#include <iostream>
+#include <string>
+#include "decisions.h"
+using namespace std;
 
 int main() 
 {
+	int grade; 
+
+	cout<<"Please enter your grade: ";
+	cin>>grade;
+
+	string if_grade = get_letter_grade_using_if(grade);
+	cout<<"Here is your grade using if statements: "<<if_grade<<endl;
+
+	string switch_grade = get_letter_grade_using_switch(grade);
+	cout<<"Here is your grade using a switch statements: "<<switch_grade<<endl;
+
 	return 0;
 }
