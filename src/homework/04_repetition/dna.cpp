e5268e72263ded57fd2e96451bcbf4faacd1bf32
lanefrasier22 <lane.frasier@g.austincc.edu>
2022-09-30 18:14:15 +0000

    hw 4 with test cases

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,32 @@
 //add include statements
+#include "dna.h"
 
 //add function(s) code here
+
+int factorial(int num)
+{
+    int total = 1;
+    int i = num;
+    while (i >0){
+        total = total * i;
+        i--;
+    }
+
+    return total;
+}
+
+
+int gcd(int num1, int num2)
+{
+    while(num1 != num2){
+        if (num1 < num2){
+            num1 += num2;
+            num2 = num1 - num2;
+            num1 -= num2; 
+        }else{
+            num1 -= num2;
+        }
+    }
+
+    return num1;
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,6 +1,8 @@
 //write include statements
-
+#include "dna.h"
+#include <iostream>
 //write using statements
+using namespace std;
 
 
 /*
@@ -12,5 +14,37 @@ user enters a y or Y.
 */
 int main() 
 {
+	int selection;
+	do{
+		cout<<"Which function would you like: \n";
+		cout<<"1 - Factorial \n";
+		cout<<"2 - Greatest Common Divisor \n";
+		cout<<"3 - Exit \n";
+		cin>>selection;
+		if(selection == 1){
+			int num;
+			cout<<"Enter a number for the factorial: ";
+			cin>>num;
+			int fact = factorial(num);
+			cout<<"Here is your factorial: "<<fact<<endl;
+			cout<<"Press any number 1 through 3 to continue: "<<endl;
+			cin>>selection;
+		}else if(selection == 2){
+			int num1, num2;
+			cout<<"Enter 2 numbers for the GCD seperated by a space: "<<endl;
+			cin>>num1>>num2;
+			int output = gcd(num1, num2);
+			cout<<"Here is your GCD: "<<output<<endl;
+			cout<<"Please enter another selection from the menu: "<<endl;
+			cin>>selection;
+		}else{
+			cout<<"Are you sure you want to exit? (Press 3 again to confirm): "<<endl;
+			cin>>selection;
+			if (selection == 3){
+				break;
+			}
+			}
+		}while(selection == 1 || selection == 2 || selection ==3);
+
 	return 0;
 }
